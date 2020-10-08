8292a33847797223818438e8c0b5588be5692a98
WenYI Lee <kenneth.wy.lee21@gmail.com>
2020-10-08 09:39:37 +0800

    Add files via upload

---
+++ CT_class B, week 3.py
@@ -0,0 +1,129 @@
+i = 0
+# i += 3
+i = i + 3
+print(i)
+i = i + 3
+print(i)
+
+i = 10
+i *= 10
+i = i *10
+print(i)
+i = i *10
+print(i)
+
+k = input("請輸入整數")
+float(k)+10
+
+import math as ma
+
+ma.ceil(3.4)
+
+# syntax
+# if boolean-expression:
+#     statement(s)
+
+1 >= 0
+
+int(1 >= 0)
+
+1 <= 0
+
+int(1 <= 0)
+
+i = -1
+if(i>=0):
+    print(i)
+    print(i)
+    print(i)
+print("hi")
+
+x = 60
+if (x>60):
+    x = x + 5
+    print(x)
+print(x)
+
+numer = 10
+demon = 0
+if(demon != 0):
+    ans = numer/ demon
+else:
+    ans = "分母為0"
+print(ans)
+
+numer = 10
+demon = 0
+if(demon != 0):
+    ans = numer/ demon
+else:
+    ans = "分母為0"
+print(ans)
+
+10%6
+
+num = 9
+if(num%2==0):
+    print(num, "是偶數")
+else:
+    print(num, "是奇數")
+
+import warnings
+warnings.filterwarnings("ignore")
+
+BF = 20
+if(BF>=30):
+    print("我不行")
+else:
+    if(BF>=25):
+        print("我勉強可以")
+    else:
+        print("我很可以")
+
+BF = 24
+if(BF>=30):
+    print("我不行")
+elif(BF>=25):
+    print("我勉強可以")
+else:
+    print("我很可以")
+
+score = 80
+if(score >= 80):
+    print("A")
+elif(score >= 70):
+    print("B")
+elif(score >= 60):
+    print("C")
+elif(score >= 50):
+    print("D")
+else:
+    print("E")
+
+score = 60
+if(score >= 80):
+    print("A")
+if(score >= 70):
+    print("B")
+if(score >= 60):
+    print("C")
+if(score >= 50):
+    print("D")
+else:
+    print("E")
+
+(1>=0)
+
+(2>0)
+
+(1>=0)and(2>0)
+
+(1>=0)and(2>3)
+
+not((1>=0)and(2>3))
+
+(1>=0) or (2>3)
+
+(0>=2) or (2>3)
+
+not((0>=2) or (2>3))
